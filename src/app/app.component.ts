e237745d9ef6d9eb05d74edb137eb3efec27b021
blindNav <2001040077@s.hanu.edu.vn>
2024-01-25 11:15:15 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [
+    HomeComponent,
+    RouterOutlet,
+    HousingLocationComponent,
+    RouterModule,
+  ],
+  //templateUrl: './app.component.html',
+  //templateUrl: './tryout.html',
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/homelogo.png" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularPrj';
+  title = 'Nav';
 }
